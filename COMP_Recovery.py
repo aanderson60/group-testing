0efb2b1f4ca57db0c99f1cdfd2ed718c5213b47c
aanderson60 <alexd43anderson@gmail.com>
2022-04-06 21:01:44 -0500

    Added documentation and modified output to include PD,DD, and specify if PD

--- COMP_Recovery.py
+++ COMP_Recovery.py
@@ -1,6 +1,19 @@
 import numpy as np
-# Recovery algorithm using the COMP algorithm
+# Recovery algorithm for group testing for a nxn test matrix using the COMP2/3 algorithm [Aldridge et. al.]
 # Optional parameter for diagonal tests
+# Args:
+#   rowTests : array of length n, the results of the row pooling top to bottom
+#   colTests : array of length n, the results of the column pooling left to right
+#   n : integer representing dimensions of nxn test matrix where the number of individuals I=n*n
+#   diagTests: array of length n, optional, the results of the diagonal pooling left to right
+# Returns:
+#   Tuple of size 3, np.array objects
+#   DND : the definite non-defective individuals
+#   PD : the probable defective individuals
+#   DD : the definite defective individuals
+# Note: all individuals are numbered starting at 0, left to right, top to bottom (i.e. for n=10, individuals are numbered 0-99)
+# (see n_array for specific format)
+#
 def COMP(rowTests,colTests,n,diagTests=None):
     # Use COMP algorithm to find set of defectives
     I = n*n
@@ -8,6 +21,8 @@ def COMP(rowTests,colTests,n,diagTests=None):
     PD = []
     # The definite non-defectives
     DND = []
+    # The definite defectives
+    DD = []
     # First determine the definite non-defectives
     for i in range(len(rowTests)):
         if rowTests[i] == 0:
@@ -20,14 +35,10 @@ def COMP(rowTests,colTests,n,diagTests=None):
             for y in range(len(colTests)):
                 if not((y*n+j) in DND):
                     DND.append(y*n+j)
+    # Generate indexing array for diagonals
+    n_array = np.arange(0, 100).reshape(10, 10)
     if diagTests != None:
         for k in range(len(diagTests)):
-            n_array = []
-            for x in range(n):
-                row = []
-                for y in range(n):
-                    row.append(x*n+y)
-                n_array.append(row)
             if diagTests[k] == 0:
                 # Add entire diag to DND list
                 diag = np.concatenate((np.diagonal(n_array,k),np.diagonal(n_array,-(n-k))))
@@ -39,8 +50,26 @@ def COMP(rowTests,colTests,n,diagTests=None):
         if not (m in DND):
             PD.append(m)
 
-    # Assume the PDs are the Definite Defectives (DD)
-    return(PD)
+    # Determine Definite Defectives (DDs) using the following fact:
+    # COMP3 cannot generate false positives for prevelance <= 2%, thus if we receive a number of positives
+    #   corresponding to a 1% of 2% prevelance, we know every element in the PDs is a DD.
+    #   Additionally, we know that a number of positives corresponding to a 3% prevelance would suggest 
+    #   a case where no false positives were generated as well - and that all PDs are once again DDs.
+    #   This method could be problematic for non exact prevelances (i.e. not a multiple of 0.01) - use tolerances for now
+
+    supPrev = len(PD)/I
+    tolerance = 1e-3
+
+    if ((supPrev - 0.01) < tolerance or (supPrev - 0.02) < tolerance or (supPrev - 0.03) < tolerance):
+        for item in PD:
+            DD.append(item)
+        PD = []
+    
+    DND = np.array(DND)
+    PD = np.array(PD)
+    DD = np.array(DD)
+
+    return((DND,PD,DD))
 
 # ---------- MAIN -----------
 rowTests = []
@@ -86,15 +115,19 @@ if diags:
     result = COMP(rowTests,colTests,n,diagTests)
 else:
     result = COMP(rowTests,colTests,n)
-result = np.array(result)
 # Add one to every index to get correct indiv number
-result += 1
+for arr in result:
+    arr += 1
 print()
 print("RESULTS:")
-print("Positive individuals: ",result,sep='')
+print("Definite Non-Defectives: ",result[0],sep='\n')
+print("Definite Defectives: ",result[2])
+print("Indeterminate Individuals: ",result[1])
+print()
 outputM = np.zeros((n,n))
 # Fill in output matrix from the positive individual list using some math
-for item in result:
+print("Definite Defective Matrix: ")
+for item in result[2]:
     outputM[int(np.floor((item-1)/n))][(item-1)%n] = 1
 
 print("Output Testing Matrix:")
